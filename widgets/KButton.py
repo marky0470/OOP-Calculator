503677a4504bec0dab136526095ae7494713fae6
marky0470 <mark.soriano04@yahoo.com>
2022-06-23 23:48:50 +0800

    the ubrub

--- widgets/KButton.py
+++ widgets/KButton.py
@@ -60,20 +60,6 @@ class KButton(ttk.Frame):
             fill=self.background,
             tags=("Frame"),
         )
-        """
-        DrawingEngine().drawRoundedRect(
-            canvas=self.canvas,
-            x1=0,
-            x2=self['width'],
-            y1=0,
-            y2=self['height'],
-            radius=20,
-            fill=self.background,
-            tags=("Frame"),
-        )
-        """
-        #self.canvas.itemconfig("Frame", fill=self.background)
-
         self.canvas.create_text(self['width'] * 0.5, self['height'] * 0.5, text=self.text, fill='black')
         self.canvas.grid(column=self.column, row=self.row, columnspan=self.columnspan, rowspan=self.rowspan)
 
